05cf8cb9d6d4e9daa05c756fbbc86d70bb80f722
NoaEdlund <noaedlund@gmail.com>
2025-12-16 08:33:21 +0100

    Här kommer teacher

---
+++ src/main/java/se/chasacademy/databaser/coursesystem/Teacher.java
@@ -0,0 +1,55 @@
+package se.chasacademy.databaser.coursesystem;
+
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+
+@Entity
+public class Teacher {
+    @Id @GeneratedValue private Long id;
+    @OneToMany (mappedBy = "teacher") private List<Courses> courses = new ArrayList<>();
+    private String firstName;
+    private String lastName;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public List<Courses> getCourses() {
+        return courses;
+    }
+
+    public void setCourses(List<Courses> courses) {
+        this.courses = courses;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    private String email;
+}
