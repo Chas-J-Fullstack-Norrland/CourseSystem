152af7fff714ad6820da02ac9bddd119235d3890
nicolaslovgren92 <nicolas.lovgren92@gmail.com>
2025-12-16 09:59:58 +0100

    Delete ParticipantRepository.java

--- src/main/java/se/chasacademy/databaser/coursesystem/repository/ParticipantRepository.java
+++
@@ -1,9 +0,0 @@
-package se.chasacademy.databaser.coursesystem.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import se.chasacademy.databaser.coursesystem.models.Room;
-
-@Repository
-public interface RoomRepository extends JpaRepository<Room,Long> {
-}
