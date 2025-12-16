82dec217df4baaa21009b9dce4d4aa8f5a4b03d1
nicolaslovgren92 <nicolas.lovgren92@gmail.com>
2025-12-16 09:59:54 +0100

    Update RoomRepository.java

---
+++ src/main/java/se/chasacademy/databaser/coursesystem/repository/RoomRepository.java
@@ -0,0 +1,9 @@
+package se.chasacademy.databaser.coursesystem.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import se.chasacademy.databaser.coursesystem.models.Room;
+
+@Repository
+public interface RoomRepository extends JpaRepository<Room,Long> {
+}
