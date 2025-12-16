4347bab3a284a54d0afd7f8ddf05d1d7b654f353
Joso1207 <joso1207@student.miun.se>
2025-12-16 08:31:18 +0100

    Room entity + repository

---
+++ src/main/java/se/chasacademy/databaser/coursesystem/model/Room.java
@@ -0,0 +1,62 @@
+package se.chasacademy.databaser.coursesystem.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Room {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String adress;
+
+    @Column(nullable = false, columnDefinition = "INT CHECK(capacity>0)")
+    private int capacity;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAdress() {
+        return adress;
+    }
+
+    public void setAdress(String adress) {
+        this.adress = adress;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public String print() {
+        return "Room{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", adress='" + adress + '\'' +
+                ", capacity=" + capacity +
+                '}';
+    }
+}
