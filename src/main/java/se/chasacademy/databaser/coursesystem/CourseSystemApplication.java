59929d751f90b9f1e59e6048d24b31fb36ce1410
Svartakatten <rikexhx@gmail.com>
2025-12-16 10:56:37 +0100

    removed trash

--- src/main/java/se/chasacademy/databaser/coursesystem/CourseSystemApplication.java
+++ src/main/java/se/chasacademy/databaser/coursesystem/CourseSystemApplication.java
@@ -1,40 +1,13 @@
 package se.chasacademy.databaser.coursesystem;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import se.chasacademy.databaser.coursesystem.repository.*;
 
 @SpringBootApplication
-public class CourseSystemApplication implements CommandLineRunner {
-    private final TeacherRepository teacherRepository;
-    private final CourseRepository courseRepository;
-    private final ParticipantRepository participantRepository;
-    private final RoomRepository roomRepository;
-    private final CourseSessionRepository sessionRepository;
-
-
-    public CourseSystemApplication(
-            TeacherRepository teacherRepository,
-            CourseRepository courseRepository,
-            ParticipantRepository participantRepository,
-            RoomRepository roomRepository,
-            CourseSessionRepository sessionRepository
-    ) {
-
-            this.teacherRepository =teacherRepository;
-            this.courseRepository =courseRepository;
-            this.participantRepository =participantRepository;
-            this.roomRepository =roomRepository;
-            this.sessionRepository =sessionRepository;
-}
+public class CourseSystemApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CourseSystemApplication.class, args);
     }
 
-	@Override
-	public void run(String... args) {
-
-	}
 }
