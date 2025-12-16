ef3f0c7814d6cd24bd83c6bcb5eb3ba6531f3197
Joso1207 <joso1207@student.miun.se>
2025-12-16 09:04:25 +0100

    Joined packages Repository

--- src/main/java/se/chasacademy/databaser/coursesystem/repository/ParticipantRepository.java
+++ src/main/java/se/chasacademy/databaser/coursesystem/repository/ParticipantRepository.java
@@ -2,8 +2,8 @@ package se.chasacademy.databaser.coursesystem.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import se.chasacademy.databaser.coursesystem.models.Participant;
+import se.chasacademy.databaser.coursesystem.models.Room;
 
 @Repository
-public interface ParticipantRepository extends JpaRepository<Participant, Long> {
+public interface RoomRepository extends JpaRepository<Room,Long> {
 }
